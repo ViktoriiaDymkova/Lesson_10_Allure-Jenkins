a17d02b25ed9508d1d73fcb5d8d2b6b4e30cd83d
user <stel-vd@yandex.ru>
2022-05-06 01:44:22 +0300

    Added allure attachment (video)

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -6,6 +6,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -16,11 +17,17 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
     @AfterEach
     void addAttachments(){
         Attach.screenshotAs("мой скриншот");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
